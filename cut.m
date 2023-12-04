c5444c7561add59943a5c1e83c17ed3306aca651
Wanna-13e <116327163+Wanna-13e@users.noreply.github.com>
2023-12-04 10:49:58 +0800

    Add files via upload

--- cut.m
+++ cut.m
@@ -1,17 +1,8 @@
-i=imread('D:\THU\Dexp\4_2!.bmp'); %读取图像
+i=imread('D:\THU\Dexp\6_3!.bmp'); %读取图像
 
-i = im2double(i);
-% j=zeros(3032,4000,3);
-% for a = 1 : 3032
-%     for b = 1: 4000
-%         for c = 1:3
-%             j(a,b,c)=i(a+3,b,c);
-%         end
-%     end
-% end
+j = i(1:end,2:3997,1:end);%裁剪图像
 
+imwrite(j,'6_02_3036_3996.bmp'); %存储图像
 
-j = i(1:end,4:3035,1:end);%裁剪图像
-imwrite(j,'01.bmp'); %存储图像
 figure,imshow(i); %显示图像
 figure,imshow(j); %显示图像
